7612dd9c888ed08e3a0fdd099d1ef925c01b1116
FluentCoding <zSuckyLP@gmail.com>
2022-02-20 00:29:29 +0100

    remove unnecessary data

--- gh-pages.js
+++ gh-pages.js
@@ -2,14 +2,6 @@ var ghpages = require('gh-pages');
 
 ghpages.publish(
     'public', // path to public directory
-    {
-        branch: 'gh-pages',
-        repo: 'https://github.com/FluentCoding/Melee-Decompilation-Website.git',
-        user: {
-            name: 'FluentCoding',
-            email: 'zsuckylp@gmail.com'
-        }
-    },
     () => {
         console.log('Deploy Complete!')
     }
